84ba693cf016d230b3af4ed0ba569dc598673ed5
sagar satpute <71960807+satputesagar@users.noreply.github.com>
2023-03-13 09:28:49 +0530

    done

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const messages = require("./routes/messages");
 const PostLike = require("./models/PostLike");
 const Post = require("./models/Post");
 
-
+dotenv.config();
 
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
@@ -26,14 +26,16 @@ io.on("connection", (socket) => socketServer(socket));
 
 mongoose.connect(
   process.env.MONGO_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  { useNewUrlParser: true,
+     useUnifiedTopology: true },
   () => {
     console.log("MongoDB connected");
   }
 );
 
-httpServer.listen(process.env.PORT || 4000, () => {
-  console.log("Listening");
+const port=process.env.PORT || 4000
+httpServer.listen(port, () => {
+  console.log(`lisetening ${port}`);
 });
 
 app.use(express.json());
@@ -44,9 +46,9 @@ app.use("/api/comments", comments);
 app.use("/api/messages", messages);
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/public")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/public", "index.html"));
   });
 }
